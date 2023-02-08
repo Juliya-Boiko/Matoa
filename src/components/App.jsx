a1306fd17de2dbc205789b234e1666bd2d587868
Juliya Boiko <boikojuliya2707@gmail.com>
2023-02-08 15:27:54 +0200

    add Hero

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { SharedLayout } from "layouts/SharedLayout";
 import { Home } from "pages/Home";
 import { News } from "pages/News";
 import { Details } from "pages/Details";
-import { lazy, Suspense } from "react";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
