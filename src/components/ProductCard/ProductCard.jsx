0d7c5bdeb8919349644576f3aa971bba21e81290
Juliya Boiko <boikojuliya2707@gmail.com>
2023-02-13 10:52:09 +0200

    add ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,11 @@
 import { priceCalculator } from "helpers/priceCalculator";
 import { useState } from "react";
 import DefaultImg from '../../images/products/default.png';
+import { ProductCardContent,
+  ProductArticle, ProductCardImg, ProductCardTitle, ProductCardPrice, ProductCardOldPrice, ProductCardNewPrice,
+  ProductCardAmountBlock, ProductCardActions, ProductCardAmount, ProductDecrBtn, ProductIncrBtn, ProductCardBtn,
+  ProductCardDescrItem, ProductCardDescrTitle, ProductCardDescrText
+} from "./ProductCard.styled";
 
 export const ProductCard = ({ product }) => {
   const [amount, setAmount] = useState(1);
@@ -21,21 +26,50 @@ export const ProductCard = ({ product }) => {
   }
 
   return (
-    <div>
-      <img src={DefaultImg} alt="" width="50px" />
-      <div>
-        <h1>{product.name}</h1>
-        <p>{product.price}</p>
-        <p>{priceCalculator(product.price, product.discount)}</p>
+    <ProductCardContent>
+      <ProductArticle>
+        <ProductCardImg src={DefaultImg} alt="" />
         <div>
-          <div>
-            <button type="button" onClick={decrHandler}>Decr</button>
-            <span>{amount}</span>
-            <button type="button" onClick={incrHandler}>Incr</button>
-          </div>
-          <button>Add</button>
+          <ProductCardTitle>{product.name}</ProductCardTitle>
+          {!product.discount && <ProductCardPrice>Rp {product.price}</ProductCardPrice>}
+          {product.discount !== 0 && <ProductCardOldPrice>Rp {product.price}</ProductCardOldPrice>}
+          {product.discount !== 0 && <ProductCardNewPrice>Rp {priceCalculator(product.price, product.discount)}</ProductCardNewPrice>}
+          <ProductCardActions>
+            <ProductCardAmountBlock>
+              <ProductDecrBtn type="button" onClick={decrHandler}>-</ProductDecrBtn>
+              <ProductCardAmount>{amount}</ProductCardAmount>
+              <ProductIncrBtn type="button" onClick={incrHandler}>+</ProductIncrBtn>
+            </ProductCardAmountBlock>
+            <ProductCardBtn type="button">Add to basket</ProductCardBtn>
+          </ProductCardActions>
         </div>
-      </div>
-    </div>
+      </ProductArticle>
+      <ul>
+        <ProductCardDescrItem>
+          <ProductCardDescrTitle>Material</ProductCardDescrTitle>
+          <ProductCardDescrText>{product.material}</ProductCardDescrText>
+        </ProductCardDescrItem>
+        <ProductCardDescrItem>
+          <ProductCardDescrTitle>Case</ProductCardDescrTitle>
+          <ProductCardDescrText>{product.case}</ProductCardDescrText>
+        </ProductCardDescrItem>
+        <ProductCardDescrItem>
+          <ProductCardDescrTitle>Movement</ProductCardDescrTitle>
+          <ProductCardDescrText>{product.movement}</ProductCardDescrText>
+        </ProductCardDescrItem>
+        <ProductCardDescrItem>
+          <ProductCardDescrTitle>Dial</ProductCardDescrTitle>
+          <ProductCardDescrText>{product.dial}</ProductCardDescrText>
+        </ProductCardDescrItem>
+        <ProductCardDescrItem>
+          <ProductCardDescrTitle>Hand</ProductCardDescrTitle>
+          <ProductCardDescrText>{product.hand}</ProductCardDescrText>
+        </ProductCardDescrItem>
+        <ProductCardDescrItem>
+          <ProductCardDescrTitle>Important to note</ProductCardDescrTitle>
+          <ProductCardDescrText>{product.important}</ProductCardDescrText>
+        </ProductCardDescrItem>
+      </ul>
+    </ProductCardContent>
   );
 };
--- src/components/ProductCard/ProductCard.styled.js
+++ src/components/ProductCard/ProductCard.styled.js
@@ -1,5 +1,175 @@
 import styled from "styled-components";
 
+export const ProductCardContent = styled.div`
+  padding: 0 0 50px 0;
+`;
+
 export const ProductArticle = styled.div`
-  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 0 25px 0;
+`;
+
+export const ProductCardImg = styled.img`
+  width: 100px;
+  margin: 0 15px 0 0;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    width: 350px;
+    margin: 0 40px 0 0;
+  }
+`;
+
+export const ProductCardTitle = styled.h1`
+  margin: 0 0 10px 0;
+  text-transform: uppercase;
+  font-size: 20px;
+  line-height: 1.2;
+  letter-spacing: 0.02em;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 50px;
+  }
+`;
+
+export const ProductCardPrice = styled.p`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 1.7;
+  letter-spacing: 0.02em;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 24px;
+  }
+`;
+
+export const ProductCardOldPrice = styled.p`
+  display: inline-block;
+  position: relative ;
+  margin: 0 0 6px 0;
+  font-weight: ${p => p.theme.fontWeight.regular};
+  font-size: 12px;
+  line-height: 1.5;
+  letter-spacing: 0.2em;
+  color: ${p => p.theme.colors.primaryLight};
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 20px;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    display: block;
+    width: 100%;
+    height: 1px;
+    background: ${p => p.theme.colors.accentRed};
+  }
+`;
+
+export const ProductCardNewPrice = styled.p`
+  margin: 0 0 25px 0;
+  font-weight: ${p => p.theme.fontWeight.medium};
+  font-size: 15px;
+  line-height: 1.2;
+  letter-spacing: 0.2em;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 24px;
+  }
+`;
+
+export const ProductCardActions = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ProductCardAmountBlock = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 20px 0 0;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    margin: 0 40px 0 0;
+  }
+`;
+
+export const ProductCardAmount = styled.span`
+  margin: 0 10px;
+  font-size: 20px;
+  letter-spacing: 0.02em;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    margin: 0 20px;
+    font-size: 30px;
+  }
+`;
+
+export const ProductDecrBtn = styled.button`
+  outline: 1px solid #777777;
+  color: #777777;
+  background-color: transparent;
+  border: none;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 30px;
+    padding: 0 15px;
+  }
+`;
+
+export const ProductIncrBtn = styled.button`
+  padding: 2px 5px;
+  color: ${p => p.theme.colors.secondary};
+  background-color: ${p => p.theme.colors.accentOrange};
+  border: none;
+  outline: none;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 30px;
+    padding: 1px 15px;
+  }
+`;
+
+export const ProductCardBtn = styled.button`
+  display: block;
+  padding: 4px 7px;
+  font-family: 'Taviraj',serif;
+  font-size: 10px;
+  line-height: 1.2;
+  letter-spacing: 0.02em;
+  border: none;
+  outline: none;
+  color: ${p => p.theme.colors.secondary};
+  background-color: ${p => p.theme.colors.accentOrange};
+  cursor: pointer;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 24px;
+    padding: 4px 20px;
+  }
+`;
+
+export const ProductCardDescrItem = styled.li`
+  &:not(:last-child) {
+    margin: 0 0 20px 0;
+  }
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    &:not(:last-child) {
+      margin: 0 0 25px 0;
+    }
+  }
+`;
+
+export const ProductCardDescrTitle = styled.p`
+  margin: 0 0 8px 0;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.2;
+  letter-spacing: 0.02em;
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 24px;
+  }
+`;
+
+export const ProductCardDescrText = styled.p`
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 1.2;
+  letter-spacing: 0.02em;
+  color: ${p => p.theme.colors.primaryAddictional};
+  @media screen and (min-width: ${p => p.theme.breakPoints.tablet}) {
+    font-size: 16px;
+  }
 `;
